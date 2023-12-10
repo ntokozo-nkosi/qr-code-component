2e0f61567a6e64c11390568677f79309a9b5de38
ntokozo-nkosi <ntokozo.nkosi257@gmail.com>
2023-12-10 22:20:56 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='Card'>
+      <img src="/image-qr-code.png" alt="Frontend Mentor QR code" />
+      <div>
+        <p className='primary-txt'>Improve your front-end skills by building projects</p>
+        <p className='secondary-txt'>Scan the QR code to visit frontend Mentor and take your coding skills to the next level</p> 
+      </div>
+    </div>
+  )
+}
+
+export default App
